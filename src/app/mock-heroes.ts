60189843183425bf6ea38b105c24da1df29559f9
Guillem Duñó Aixerch <guillemduno@gmail.com>
2018-07-20 15:58:12 +0200

    hero service created

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import {Hero} from "./hero";
+
+export const HEROES:Hero[] = [
+    {id: 1, name:"Batman"},
+    {id: 2, name:"CatWomen"},
+    {id: 3, name:"Joker"},
+    {id: 4, name:"Penguin"},
+    {id: 5, name:"Superman"},
+    {id: 6, name:"He-Man"},
+];
