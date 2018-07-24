9060e22900a8ade1479f936a6aee1df41d35f7bd
Guillem Duñó Aixerch <guillemduno@gmail.com>
2018-07-24 10:08:46 +0200

    service plus message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,7 +15,7 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) { }
   
   ngOnInit() {
-    
+    this.getHeroes();
   }
   
   onSelect(hero:Hero):void{
@@ -23,7 +23,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes():void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
