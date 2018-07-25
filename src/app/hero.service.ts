d3257003ad5ea2230b9efd642b67fa057f2410ab
Guillem Duñó Aixerch <guillemduno@gmail.com>
2018-07-25 10:00:27 +0200

    allRouting done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+
+import {Observable, of} from "rxjs";
 import{Hero} from "./hero";
 import {HEROES} from "./mock-heroes";
-import {Observable, of} from "rxjs";
-
 import {MessageService} from "./message.service";
 
 @Injectable({
@@ -10,14 +10,15 @@ import {MessageService} from "./message.service";
 })
 export class HeroService {
 
-
   constructor(private messageService: MessageService) { }
-
- /* getHeroes():Hero[]{
-    return HEROES;
-  }*/
   getHeroes(): Observable<Hero[]>{
-    this.messageService.add("HeroService : fetched heroes");
+    this.messageService.add('HeroService : fetched heroes');
     return of (HEROES);
   }
+  getHero(id: number): Observable<Hero>{
+    this.messageService.add(`HeroService : fetched hero id=${id}`);
+    return of (HEROES.find(hero => hero.id === id));
+  }
+
+
 }
